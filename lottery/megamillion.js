0932dbe07359c3d5993f701f1cd6dc79e10edd90
Eric <eric.e.nicolas@gmail.com>
2018-10-15 01:31:51 -0700

    lol

--- lottery/megamillion.js
+++ lottery/megamillion.js
@@ -1,7 +1,4 @@
 
-
-
-
 var request = require('request');
 const cheerio = require('cheerio');
 var number =[];
@@ -10,16 +7,18 @@ sorted =[[0,"-"],[1,"-"],[2,"-"],[3,"-"],[4,"-"],[5,"-"],[6,"-"],[7,"-"],[8,"-"]
 known = [] ;
 known1 = [];
 
-for (var x = 0; x<70; x++){
+var final = [];
+
+for (var x = 0; x<71; x++){
   number.push (0);
 }
-for (var x = 0; x<27; x++){
+for (var x = 0; x<26; x++){
   ball.push (0);
 }
-request('https://www.lotteryusa.com/powerball/pb-year.html', function (error, response, body) {
+request('https://www.national-lottery.com/megamillions/past-results', function (error, response, body) {
   var $ = cheerio.load(body);
-  var area = $("li");
-  var aream = $(".powerball");
+  var area = $(".megamillions-ball");
+  var aream = $(".megamillions-mega-ball");
     area.each((index,data) => {
     var position = parseInt($(data).text());
     number[position] = number[position] + 1;
@@ -31,7 +30,7 @@ request('https://www.lotteryusa.com/powerball/pb-year.html', function (error, re
     var position = parseInt($(data).text());
     ball[position] = ball[position] + 1;
   })
-  for (var x = 0;  x < 27; x++){
+  for (var x = 0;  x < 26; x++){
     console.log(x +" ball ::"+ ball[x])
   }
   for (var x = 1; x < number.length; x++){
@@ -39,42 +38,52 @@ request('https://www.lotteryusa.com/powerball/pb-year.html', function (error, re
     sorted[number[x]].push(x);
   }
   
-  console.log(sorted);
 
+console.log(sorted);
   for(var i = 0; i < 20;i++){
     pick =[];
 
     for (var y = 0 ; y < 3;y++){
    
-      var first = Math.floor((Math.random() * 12) + 9);
+      var first = Math.floor((Math.random() * 8) + 4);
+    
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 12) + 9);
+        first = Math.floor((Math.random() * 8) + 4);
       }
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 12) + 9);
+        first = Math.floor((Math.random() * 8) + 4);
       }
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 12) + 9);
+        first = Math.floor((Math.random() * 8) + 4);
       }
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 12) + 9);
+        first = Math.floor((Math.random() * 8) + 4);
       }
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 12) + 9);
+        first = Math.floor((Math.random() * 8) + 4);
       }
+     
       var second = Math.floor((Math.random() * (sorted[first].length -2)) +2); 
        pick.push(sorted[first][second]);
+   
       }
       for (var y = 0 ; y < 2;y++){
-        var first = Math.floor((Math.random() * 6) + 3); 
+        var first = Math.floor((Math.random() * 3) + 1); 
         var second = Math.floor((Math.random() * (sorted[first].length-2))+2); 
          pick.push(sorted[first][second]);
         }
-        pick.push(Math.floor(Math.random() * 26) + 1)
-        console.log(pick);
+        pick.sort(function(a, b) {
+          return a - b;
+        });
+        pick.push(Math.floor(Math.random() * 25) + 1)
+        final.push(pick);
          
   }
-
+  // for (var i = 0; i < 5; i ++){
+  //   var pin=  Math.floor((Math.random() * 20) +0); 
+  //   console.log(final[pin]);
+  // }
+  console.log(final);
 })
 
 
--- lottery/powerball.js
+++ lottery/powerball.js
@@ -1,4 +1,7 @@
 
+
+
+
 var request = require('request');
 const cheerio = require('cheerio');
 var number =[];
@@ -7,16 +10,16 @@ sorted =[[0,"-"],[1,"-"],[2,"-"],[3,"-"],[4,"-"],[5,"-"],[6,"-"],[7,"-"],[8,"-"]
 known = [] ;
 known1 = [];
 
-for (var x = 0; x<71; x++){
+for (var x = 0; x<70; x++){
   number.push (0);
 }
-for (var x = 0; x<26; x++){
+for (var x = 0; x<27; x++){
   ball.push (0);
 }
-request('https://www.national-lottery.com/megamillions/past-results', function (error, response, body) {
+request('https://www.lotteryusa.com/powerball/pb-year.html', function (error, response, body) {
   var $ = cheerio.load(body);
-  var area = $(".megamillions-ball");
-  var aream = $(".megamillions-mega-ball");
+  var area = $("li");
+  var aream = $(".powerball");
     area.each((index,data) => {
     var position = parseInt($(data).text());
     number[position] = number[position] + 1;
@@ -28,7 +31,7 @@ request('https://www.national-lottery.com/megamillions/past-results', function (
     var position = parseInt($(data).text());
     ball[position] = ball[position] + 1;
   })
-  for (var x = 0;  x < 26; x++){
+  for (var x = 0;  x < 27; x++){
     console.log(x +" ball ::"+ ball[x])
   }
   for (var x = 1; x < number.length; x++){
@@ -43,35 +46,35 @@ request('https://www.national-lottery.com/megamillions/past-results', function (
 
     for (var y = 0 ; y < 3;y++){
    
-      var first = Math.floor((Math.random() * 8) + 4);
-    
+      var first = Math.floor((Math.random() * 12) + 9);
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 8) + 4);
+        first = Math.floor((Math.random() * 12) + 9);
       }
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 8) + 4);
+        first = Math.floor((Math.random() * 12) + 9);
       }
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 8) + 4);
+        first = Math.floor((Math.random() * 12) + 9);
       }
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 8) + 4);
+        first = Math.floor((Math.random() * 12) + 9);
       }
       if(sorted[first].length < 3){
-        first = Math.floor((Math.random() * 8) + 4);
+        first = Math.floor((Math.random() * 12) + 9);
       }
-     
       var second = Math.floor((Math.random() * (sorted[first].length -2)) +2); 
        pick.push(sorted[first][second]);
-   
       }
       for (var y = 0 ; y < 2;y++){
-        var first = Math.floor((Math.random() * 3) + 1); 
+        var first = Math.floor((Math.random() * 6) + 3); 
         var second = Math.floor((Math.random() * (sorted[first].length-2))+2); 
          pick.push(sorted[first][second]);
         }
-        pick.push(Math.floor(Math.random() * 25) + 1)
-        console.log('pick: '+pick);
+        pick.sort(function(a, b) {
+          return a - b;
+        });
+        pick.push(Math.floor(Math.random() * 26) + 1)
+        console.log(pick);
          
   }
 
